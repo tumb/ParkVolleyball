e752013b330cf8418653f145012749c9c10047ec
Tumb <thom.burnett@gmail.com>
2023-05-16 06:56:46 -0400

    Test change

--- components/UI/ScheduleForm.tsx
+++ components/UI/ScheduleForm.tsx
@@ -19,7 +19,7 @@ export default function ScheduleForm({
     <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-lg">
         <h1 className="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">
-          Find your schedule
+          Find Schedule
         </h1>
 
         <p className="mx-auto mt-4 max-w-md text-center text-gray-500">
