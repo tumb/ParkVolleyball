4cc382903db5169b61a7e492a5ae8fe8bb34bf87
anindomac <sarkeranindo@gmail.com>
2023-05-16 17:57:55 +0600

    league id search ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
+import HeroCta from "@/components/UI/HeroCta";
+import LeagueForm from "@/components/UI/LeagueForm";
 import ScheduleForm from "@/components/UI/ScheduleForm";
-import ScheduleTable from "@/components/UI/Table";
 import React from "react";
 
 export default function Home() {
   return (
     <div>
-      {/* <ScheduleForm /> */}
-      <div className="mx-auto px-32 py-16"></div>
+      <HeroCta />
+      <LeagueForm />
     </div>
   );
 }
