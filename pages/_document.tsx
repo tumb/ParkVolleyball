9f1c350fd09053db4c300f0100aa6d32d6f2b2cc
Tumb <thom.burnett@gmail.com>
2023-08-11 08:02:18 -0400

    Can't see a difference in the tags but this one compiles and the other didn't.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>  
-		<script defer data-domain="parkvball.com" src="https://plausible.io/js/script.js"></script>    
+      <Head>
+	    <script defer data-domain="parkvball.com" src="https://plausible.io/js/script.js"></script>
 	  </Head>
       <body>
         <Main />
