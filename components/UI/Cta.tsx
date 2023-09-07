d111ed377144227001666a3a1da45f9518138145
Tumb <thom.burnett@gmail.com>
2023-09-07 17:56:08 -0400

    Made new names for images. It compiled locally but not on Vercel.

--- components/UI/Cta.tsx
+++ components/UI/Cta.tsx
@@ -3,9 +3,9 @@ import LeagueForm from "./LeagueForm";
 import parkImage from "../assets/park.png";
 import childImage from "../assets/Child_watching.png";
 import iconImage from "../assets/icon-512x512.png";
-import iconImage from "../assets/Shelagh_2023.png";
-import iconImage from "../assets/Players.png";
-import iconImage from "../assets/Jakki_Andy_2022.png";
+import shelaghImage from "../assets/Shelagh_2023.png";
+import playersImage from "../assets/Players.png";
+import jakkiImage from "../assets/Jakki_Andy_2022.png";
 
 export default function Cta() {
   return (
